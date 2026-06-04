07e35d86b9c1508bee3f036e9e0e18cf4cfc785d
lazuardiyasfin <70686532+lazuardiyasfin@users.noreply.github.com>
2026-06-04 13:44:29 +0700

    chore: implement websocket server for build validation

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,6 +1,26 @@
-def main() -> None:
-    print("Hello from backend!")
+import asyncio
+
+import websockets
+
+
+async def echo(websocket: websockets.ServerConnection) -> None:
+    print("Client connected")
+    try:
+        async for message in websocket:
+            if isinstance(message, bytes):
+                message = message.decode("utf-8")
+
+            print(f"Received message: {message}")
+            await websocket.send(f"{message}")
+    except websockets.ConnectionClosed:
+        print("Client terputus")
+
+
+async def main() -> None:
+    async with websockets.serve(echo, "", 6767) as server:
+        print("WebSocket server started on ws://localhost:6767")
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
