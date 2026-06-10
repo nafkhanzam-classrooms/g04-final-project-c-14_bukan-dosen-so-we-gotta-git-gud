6b568ac497edc658448dc266a766439dceedcfce
ilhmpbta <178406185+ilhmpbta@users.noreply.github.com>
2026-06-10 17:50:27 +0700

    feat: front-end routing

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -2,7 +2,28 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('@/features/landing/LandingView.vue'),
+    },
+    {
+      path: '/join',
+      name: 'join',
+      component: () => import('@/features/join/StudentJoinView.vue'),
+    },
+    {
+      path: '/host',
+      name: 'host',
+      component: () => import('@/features/host/HostDashboardView.vue'),
+    },
+    {
+      path: '/room/:roomId',
+      name: 'room',
+      component: () => import('@/features/room/RoomView.vue'),
+    },
+  ],
 })
 
 export default router
